ed489680b9adfd6f1a01621674f40d5a91384acf
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-07-28 15:20:01 -0400

    feed styles added

---
+++ components/feedPage/feedPage.styles.js
@@ -0,0 +1,41 @@
+
+import { StyleSheet } from 'react-native'
+
+
+export default styles = StyleSheet.create({
+  
+    header: {
+      height: 70,
+    },
+
+    card: {
+      marginRight: 0,
+      marginLeft: 0,
+    },
+
+    postImage: {
+      flex: 1,
+      height: 300,
+    },
+
+    cardHeader: {
+        height: 70,
+        paddingTop: 15,
+        marginBottom: 0
+    },
+
+    cardFooter: {
+        height: 50,
+    },
+
+    cardFooterLeft: {
+      flexDirection: 'column',
+      justifyContent: 'flex-start'
+    },
+    
+    cardFooterText: {
+    }
+
+});
+
+
