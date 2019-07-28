581edc88763437c6c35eb9ad107275dd68a58848
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-07-28 15:27:40 -0400

    sports component added

---
+++ Bender-Sports-App/components/sportsPage/sportsPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native';
+
+
+export default class sportsPage extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Sports Screen</Text>
+      </View>
+    )
+  }
+}
