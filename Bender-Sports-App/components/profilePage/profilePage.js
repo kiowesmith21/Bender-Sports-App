86da1606b725caafc473b85bda77365f726bbb52
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-07-28 15:26:55 -0400

    profile component added

---
+++ Bender-Sports-App/components/profilePage/profilePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native';
+
+
+export default class profilePage extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+      </View>
+    )
+  }
+}
