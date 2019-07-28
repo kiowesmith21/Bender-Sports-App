0ee486234a70caedd4ac37b0add9298324e91124
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-07-28 15:25:42 -0400

    post component added

---
+++ Bender-Sports-App/components/postPage/postPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native';
+
+
+export default class postPage extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Post Screen</Text>
+      </View>
+    )
+  }
+}
